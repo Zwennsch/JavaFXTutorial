c58c91d9c7e76dbf71b2e6179cd9e7c1aabf2982
Sven Schroeder <schuen@gmx>
2020-02-17 19:04:31 +0100

    first part: button, textfield, text, bindings

--- src/main/java/com/sven/java/fx/jfxTutorial/App.java
+++ src/main/java/com/sven/java/fx/jfxTutorial/App.java
@@ -4,6 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,22 +22,30 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 840, 480);
-        stage.setScene(scene);
-        stage.show();
+        stage.setScene(new Scene(createContent()));
+    	stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    private Parent createContent() {
+    	VBox root = new VBox();
+    	root.setPrefSize(600, 480);
+    	
+    	TextField input = new TextField();
+    	input.setFont(new Font(20));
+    	Text output = new Text();
+    	
+    	output.textProperty().bindBidirectional(input.textProperty());
+    	
+    	Button button = new Button("Press");
+//    	button.setOnAction(e -> {
+//    		output.setText(input.getText());
+//    	});
+    	root.getChildren().addAll(input, button,output);
+    	return root;
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
